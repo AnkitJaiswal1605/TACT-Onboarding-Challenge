0dcad4cc060adae3ffff928b303ea889cf67ae40
Shashank <shashankgupta9248@gmail.com>
2023-11-07 00:22:50 +0530

    shashank tact challenge

---
+++ ShashankGupta_Tact_Challenge/node_modules/@tact-lang/compiler/dist/utils/crc32.d.ts
@@ -0,0 +1,1 @@
+export declare function crc32(bytes: Uint8Array, crc?: number): number;
---
+++ ShashankGupta_Tact_Challenge/node_modules/ton-x/node_modules/ton/dist/utils/base32.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="node" />
+export declare function base32Encode(buffer: Buffer): string;
+export declare function base32Decode(input: string): Buffer;
